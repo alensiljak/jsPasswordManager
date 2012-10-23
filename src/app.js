85a0f30b7e4e977085b0f8a9fe541dda7fb050cf
Alen Siljak <alen.siljak@gmail.com>
2012-10-23 13:26:53 +0200

    adding container for encryption demo; splitting tasks

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 
 // use jquery?
 
+// data:
 // todo: offer Create New Database
 // todo: offer option to import data from JSON
+
+// encryption:
 // todo: prompt for password to decrypt
 // todo: decrypt data, 
+
+// functionality
 // todo: offer Add New, Search, Edit, Delete
+
+// data
 // todo: offer Export data to JSON
